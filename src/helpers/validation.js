436be2b5540ec3040569a7108f0ceaab7061d832
LuanSALima <luanlimadev@gmail.com>
2021-04-14 16:06:18 -0300

    Fixed Tags from Featured Post and Limited File Upload to 3MB

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -12,6 +12,10 @@ module.exports.validateImage = (file) => {
     	throw new Error("La imagen no se encontró en el nombre 'imagen'");
     }
 
+    if(file.image.size > 3145728) {
+    	throw new Error("El archivo es demasiado grande. El máximo es 3 MB");
+    }
+
     const validMimetypes = ["image/jpeg", "image/png"];
 
 	if(validMimetypes.indexOf((file.image).mimetype) === -1){
